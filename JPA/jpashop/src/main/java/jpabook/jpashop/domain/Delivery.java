24e83d75e68f4c6c449590ea7471079ac30ec11b
kimjinwook1 <87925272+kimjinwook1@users.noreply.github.com>
2022-01-10 20:15:11 +0900

    값 타입 매핑

--- JPA/jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
+++ JPA/jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -14,12 +14,40 @@ public class Delivery extends BaseEntity {
     @OneToOne(mappedBy = "delivery", fetch = LAZY)
     private Order order;
 
-    private String city;
-    private String street;
-    private String zipcode;
+    @Embedded
+    private Address address;
+
     private DeliveryStatus status;
+
+    public Long getId() {
+        return id;
+    }
 
     public Order getOrder() {
         return order;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public DeliveryStatus getStatus() {
+        return status;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public void setStatus(DeliveryStatus status) {
+        this.status = status;
     }
 }
--- JPA/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ JPA/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,9 +1,8 @@
 package jpabook.jpashop.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Member extends BaseEntity {
@@ -11,49 +10,43 @@ public class Member extends BaseEntity {
     @Id @GeneratedValue
     @Column(name = "MEMBER_ID")
     private Long id;
-
     private String name;
-    private String city;
-    private String street;
-    private String zipcode;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
 
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
+    public Address getAddress() {
+        return address;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
 
-    public String getCity() {
-        return city;
+    public void setAddress(Address address) {
+        this.address = address;
     }
 
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
     }
 }
