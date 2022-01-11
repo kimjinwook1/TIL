7d937967b28a1e99252ee60a7a7f63f73cc2f9a3
kimjinwook1 <87925272+kimjinwook1@users.noreply.github.com>
2022-01-11 17:09:50 +0900

    객체지향 쿼리언어

---
+++ JPA/jpql/src/main/java/jpql/JpaMain.java
@@ -0,0 +1,65 @@
+package jpql;
+
+import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Team teamA = new Team();
+            teamA.setName("teamA");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("teamB");
+            em.persist(teamB);
+
+
+            Member member1 = new Member();
+            member1.setTeam(teamA);
+            member1.setUsername("회원1");
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setTeam(teamA);
+            member2.setUsername("회원2");
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setTeam(teamB);
+            member3.setUsername("회원3");
+            em.persist(member3);
+
+            int resultCount = em.createQuery("update Member m set m.age = 20")
+                    .executeUpdate();
+
+            em.clear();
+
+            Member findMember = em.find(Member.class, member1.getId());
+
+            System.out.println("findMember.getAge() = " + findMember.getAge());
+
+            tx.commit();
+
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+}
+
---
+++ JPA/jpql/src/main/java/jpql/Product.java
@@ -0,0 +1,42 @@
+package jpql;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private int stockAmount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStockAmount() {
+        return stockAmount;
+    }
+
+    public void setStockAmount(int stockAmount) {
+        this.stockAmount = stockAmount;
+    }
+}
